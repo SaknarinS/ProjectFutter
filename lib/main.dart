a3ef48dd5e28a3ff080cf67f4e56dce82e104d33
SaknarinS <saknarin.s@ku.th>
2020-09-08 00:13:45 -0700

    R1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
        body: Container(
         color: Colors.red[50],
         child: TestWidget(),
-        height: 600,
+        height: 800,
         width: 500,
       )
     ),
@@ -23,11 +23,37 @@ class MyApp extends StatelessWidget {
 class TestWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-   return Image.network(
-     "https://www.vippng.com/png/detail/22-220768_re-zero-rem-anime-png-rem-anime-png.png",
-     scale: 1.0,
-    
+   return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      Container(
+        height: 100.0,
+        width: 50.0,
+        color: Colors.red[500],
+      ),
+      Container(
+        height: 100.0,
+        width: 50.0,
+        color: Colors.green[400],
+      ),
+      Container(
+        height: 100.0,
+        width: 50.0,
+        color: Colors.yellow[300],
+      ),
+      Container(
+        height: 100.0,
+        width: 50.0,
+        color: Colors.blue[300],
+      ),
+      Container(
+        height: 100.0,
+        width: 50.0,
+        color: Colors.pink[200],
+      ),
+
+    ],  
    );
-    
   }
 }
